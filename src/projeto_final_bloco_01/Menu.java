3158eebdadc135c49d5855481b6c1d28e22546c8
Maria Luiza <maluduartetorres@gmail.com>
2025-05-07 14:13:00 -0300

    :sparkles: Construção das Classes Menu e Cores

---
+++ src/projeto_final_bloco_01/Menu.java
@@ -0,0 +1,62 @@
+package projeto_final_bloco_01;
+
+import java.util.Scanner;
+import projeto_final_bloco_01.util.Cores;
+
+public class Menu {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println(Cores.ANSI_PURPLE_BACKGROUND_BRIGHT + Cores.TEXT_WHITE + "*******************************************" + Cores.TEXT_RESET);
+            System.out.println(Cores.TEXT_CYAN + "                                           ");
+            System.out.println("        🕯️ E-COMMERCE DE VELAS 🕯️         ");
+            System.out.println("                                           ");
+            System.out.println(Cores.ANSI_PURPLE_BACKGROUND_BRIGHT + Cores.TEXT_WHITE + "*******************************************" + Cores.TEXT_RESET);
+            System.out.println("                                           ");
+            System.out.println(Cores.TEXT_WHITE + "1 - Cadastrar vela");
+            System.out.println("2 - Listar todas as velas");
+            System.out.println("3 - Buscar vela por ID");
+            System.out.println("4 - Atualizar vela");
+            System.out.println("5 - Deletar vela");
+            System.out.println("0 - Sair");
+            System.out.println("                                           ");
+            System.out.println(Cores.ANSI_PURPLE_BACKGROUND_BRIGHT + Cores.TEXT_WHITE + "*******************************************" + Cores.TEXT_RESET);
+            System.out.print(Cores.TEXT_YELLOW_BRIGHT + "Escolha uma opção: " + Cores.TEXT_RESET);
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println(Cores.TEXT_GREEN + "Cadastrar vela..." + Cores.TEXT_RESET);
+                    break;
+                case 2:
+                    System.out.println(Cores.TEXT_GREEN + "Listar velas..." + Cores.TEXT_RESET);
+                    break;
+                case 3:
+                    System.out.println(Cores.TEXT_GREEN + "Buscar por ID..." + Cores.TEXT_RESET);
+                    break;
+                case 4:
+                    System.out.println(Cores.TEXT_GREEN + "Atualizar vela..." + Cores.TEXT_RESET);
+                    break;
+                case 5:
+                    System.out.println(Cores.TEXT_GREEN + "Deletar vela..." + Cores.TEXT_RESET);
+                    break;
+                case 0:
+                    System.out.println(Cores.TEXT_WHITE + "Programa finalizado!" + Cores.TEXT_RESET);
+                    break;
+                default:
+                    System.out.println(Cores.TEXT_YELLOW_BRIGHT + "Opção inválida!" + Cores.TEXT_RESET);
+                    break;
+            }
+
+        } while (opcao != 0);
+
+        System.out.println("\nDesenvolvido por Maria Luiza Torres");
+        System.out.println("GitHub: github.com/MariaLuiza-dev");
+
+        scanner.close();
+    }
+}
---
+++ src/projeto_final_bloco_01/util/Cores.java
@@ -0,0 +1,13 @@
+package projeto_final_bloco_01.util;
+
+public class Cores {
+
+    public static final String TEXT_RESET = "\u001B[0m";
+
+    public static final String TEXT_WHITE = "\u001B[37m";
+    public static final String TEXT_CYAN = "\u001B[36m";
+    public static final String TEXT_YELLOW_BRIGHT = "\033[0;93m";
+    public static final String TEXT_GREEN = "\u001B[32m";
+
+    public static final String ANSI_PURPLE_BACKGROUND_BRIGHT = "\033[0;105m";
+}
